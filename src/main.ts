b29901037560a2e9cac6124c22c31c6da288b145
Ramiro Tepehua <rtcface@gmail.com>
2022-11-11 15:42:55 -0600

    add https

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs  from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule,{ httpsOptions });
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get('PORT', { infer: true });
