7ced7964591befe78ca07956bb40c70d5d1d441e
Ramiro Tepehua <rtcface@gmail.com>
2022-11-14 14:02:03 -0600

    mod main

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 const fs = require('fs');
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./../certs/private-key.pem'),
-    cert: fs.readFileSync('./../certs/public-certificate.pem'),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync('./../certs/private-key.pem'),
+  //   cert: fs.readFileSync('./../certs/public-certificate.pem'),
+  // };
   const app = await NestFactory.create(AppModule);//,{ httpsOptions }
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
