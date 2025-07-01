7fda7933ca10d6d39c7e09cff29cb5c6e7297b1f
Ramiro Tepehua <rtcface@gmail.com>
2025-07-01 11:46:44 -0600

    se cambnia el metodo de para el ssl

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 // definimos la ruta
-const crPath = '/backend-oic/certs/public-certificate.pem';
+/* const crPath = '/backend-oic/certs/public-certificate.pem';
 const pkPath = '/backend-oic/certs/keyfile-encrypted.key';
-const options: any = {};
+const options: any = {}; */
 
 
 
@@ -15,17 +15,17 @@ const options: any = {};
 async function bootstrap() {
  
   // validamos si los archivos existen
-if (fs.existsSync(pkPath) && fs.existsSync(crPath)) {
+/* if (fs.existsSync(pkPath) && fs.existsSync(crPath)) {
   // cargamos los archivos sobre las options
   options.httpsOptions = {
     key:fs.readFileSync(pkPath),
     passphrase: process.env.PASSPHRASE_SSL,
     cert:fs.readFileSync(crPath),
   }
-}
+} */
 
 
-  const app = await NestFactory.create(AppModule,options);//,{ httpsOptions }
+  const app = await NestFactory.create(AppModule);//,{ httpsOptions },options
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get('PORT', { infer: true });
